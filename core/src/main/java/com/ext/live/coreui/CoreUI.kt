2ee2e00a59ebd1c6373fe6c6fd03b631cea27595
Ajay Kumar <ajay.kumar@urbanft.com>
2026-06-30 13:54:47 +0530

    Exception handling

--- core/src/main/java/com/ext/live/coreui/CoreUI.kt
+++ core/src/main/java/com/ext/live/coreui/CoreUI.kt
@@ -5,6 +5,11 @@ import com.exe.engine.ContourEngine
 class CoreUI {
 
     fun callSdk() {
-        ContourEngine.process()
+        try {
+            ContourEngine.process()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+            println("---- ${e.message}")
+        }
     }
 }
