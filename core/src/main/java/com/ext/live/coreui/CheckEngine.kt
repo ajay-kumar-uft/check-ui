dc3eff35c971a0aba5eff0f11a2119d35b047618
Ajay Kumar <ajay.kumar@urbanft.com>
2026-09-07 18:00:20 +0530

    Engine changed

--- core/src/main/java/com/ext/live/coreui/CheckEngine.kt
+++ core/src/main/java/com/ext/live/coreui/CheckEngine.kt
@@ -2,9 +2,12 @@ package com.ext.live.coreui
 
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
 import com.exe.engine.DocumentCallback
 import com.exe.engine.DocumentEngine
 import com.exe.engine.DocumentType
+import com.exe.engine.models.DocumentInputModel
+import com.exe.engine.models.DocumentResultModel
 
 internal data class CheckRequest(val context: Context)
 
@@ -12,16 +15,17 @@ internal object CheckStarted
 
 internal object CheckEvent
 
-internal class CheckEngine : DocumentEngine<CheckRequest, CheckStarted, CheckEvent> {
+internal class CheckEngine : DocumentEngine {
 
     override val documentType: DocumentType = DocumentType.CHECK
 
     override fun process(
-        model: CheckRequest,
-        callback: DocumentCallback<CheckStarted, CheckEvent>
+        model: DocumentInputModel,
+        callback: DocumentCallback
     ) {
         println("------ process CheckEngine")
         model.context.startActivity(Intent(model.context, CheckCaptureActivity::class.java))
-        callback.onResult(CheckStarted)
+        val result = DocumentResultModel("front", "rear")
+        callback.onResult(result)
     }
 }
